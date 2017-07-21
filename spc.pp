074f3bbcd9899d98c49e6f642ded5a092f2d1287
andrewh1978 <andrew.hill@joyent.com>
2017-07-21 15:43:12 +0000

    add spc.pp

---
+++ spc.pp
@@ -0,0 +1,15 @@
+exec { "mget-profile":
+  command => "/root/sup/mget_if_changed.sh /joyentsup/stor/sup/profile.spc /root/.profile",
+}
+
+exec { "mget-sdc-config":
+  command => "/root/sup/mget_if_changed.sh /joyentsup/stor/sup/sdc-config.json /opt/local/lib/toolbox/node_modules/sdc/etc/config.json",
+}
+
+exec { "mget-sup-notify-dc":
+  command => "/root/sup/mget_if_changed.sh /joyentsup/stor/sup/spc.json /opt/local/lib/node_modules/sup-notify/etc/dc.json",
+}
+
+exec { "mget-im-notices":
+  command => "/root/sup/mget_if_changed.sh /joyentsup/stor/sup/im-notices.json /opt/local/lib/node_modules/im-notices/etc/config.json",
+}
