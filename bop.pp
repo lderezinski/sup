65747be8c1b0d3f1a8e0aa02fb95bf2222e2eabb
Linda Derezinski <linda@joyent.com>
2017-11-28 11:59:39 -0500

    testing deploy bop

--- bop.pp
+++ bop.pp
@@ -117,14 +117,14 @@ exec { "mget-thothRun":
 }
 exec { "install-manta-hk":
   require => [ Package["gcc49"], Package["gmake"] ],
-  command => "/usr/bin/cd /opt && /opt/local/bin/git clone git@github.com:joyent/manta-hk.git && /usr/bin/cd /opt/manta-hk && npm install",
+  command => "/usr/bin/cd /opt && /opt/local/bin/git clone git@github.com:joyent/manta-hk.git && /usr/bin/cd /opt/manta-hk && /opt/local/bin/npm install",
   onlyif => '/usr/bin/test ! -e /opt/manta-hk/README.md',
 }
 
 
 exec { "pull-manta-hk":
   require => [ Package["gcc49"], Package["gmake"] ],
-  command => "/usr/bin/cd /opt/manta-hk && /opt/local/bin/git pull  && npm install",
+  command => "/usr/bin/cd /opt/manta-hk && /opt/local/bin/git pull  && /opt/local/bin/npm install",
   onlyif => '/usr/bin/test -e /opt/manta-hk/README.md',
 }
 
