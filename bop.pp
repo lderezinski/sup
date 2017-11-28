122cfab8aa6cc9b803945a251067466b30ad2fa6
Linda Derezinski <linda@joyent.com>
2017-11-28 10:44:34 -0500

    testing deploy bop

--- bop.pp
+++ bop.pp
@@ -17,86 +17,86 @@ file { "/opt/BOP":
 }
 
 exec { "mget-BOP-019":
-  command => "/root/sup/mget_if_changed.sh /linda/stor/BOP/BOP-019 /opt/BOP/BOP-019",
+  command => "/root/sup/mget_if_changed.sh /joyentsup/stor/BOP/BOP-019 /opt/BOP/BOP-019",
 }
 
 exec { "mget-adminRun":
-  command => "/root/sup/mget_if_changed.sh /linda/stor/BOP/adminRun /opt/BOP/adminRun",
+  command => "/root/sup/mget_if_changed.sh /joyentsup/stor/BOP/adminRun /opt/BOP/adminRun",
 }
 
 
 exec { "mget-checkChronos":
-  command => "/root/sup/mget_if_changed.sh /linda/stor/BOP/checkChronos /opt/BOP/checkChronos",
+  command => "/root/sup/mget_if_changed.sh /joyentsup/stor/BOP/checkChronos /opt/BOP/checkChronos",
 }
 
 
 exec { "mget-checkHourly":
-  command => "/root/sup/mget_if_changed.sh /linda/stor/BOP/checkHourly /opt/BOP/checkHourly",
+  command => "/root/sup/mget_if_changed.sh /joyentsup/stor/BOP/checkHourly /opt/BOP/checkHourly",
 }
 
 
 exec { "mget-copy2manta":
-  command => "/root/sup/mget_if_changed.sh /linda/stor/BOP/copy2manta /opt/BOP/copy2manta",
+  command => "/root/sup/mget_if_changed.sh /joyentsup/stor/BOP/copy2manta /opt/BOP/copy2manta",
 }
 
 
 exec { "mget-echeckHourly":
-  command => "/root/sup/mget_if_changed.sh /linda/stor/BOP/echeckHourly /opt/BOP/echeckHourly",
+  command => "/root/sup/mget_if_changed.sh /joyentsup/stor/BOP/echeckHourly /opt/BOP/echeckHourly",
 }
 
 
 exec { "mget-elapseManta":
-  command => "/root/sup/mget_if_changed.sh /linda/stor/BOP/elapseManta /opt/BOP/elapseManta",
+  command => "/root/sup/mget_if_changed.sh /joyentsup/stor/BOP/elapseManta /opt/BOP/elapseManta",
 }
 
 
 exec { "mget-idhash.sh":
-  command => "/root/sup/mget_if_changed.sh /linda/stor/BOP/idhash.sh /opt/BOP/idhash.sh",
+  command => "/root/sup/mget_if_changed.sh /joyentsup/stor/BOP/idhash.sh /opt/BOP/idhash.sh",
 }
 
 
 exec { "mget-jpcRun":
-  command => "/root/sup/mget_if_changed.sh /linda/stor/BOP/jpcRun /opt/BOP/jpcRun",
+  command => "/root/sup/mget_if_changed.sh /joyentsup/stor/BOP/jpcRun /opt/BOP/jpcRun",
 }
 
 
 exec { "mget-lindaRun":
-  command => "/root/sup/mget_if_changed.sh /linda/stor/BOP/lindaRun /opt/BOP/lindaRun",
+  command => "/root/sup/mget_if_changed.sh /joyentsup/stor/BOP/lindaRun /opt/BOP/lindaRun",
 }
 
 
 exec { "mget-mantaRun":
-  command => "/root/sup/mget_if_changed.sh /linda/stor/BOP/mantaRun /opt/BOP/mantaRun",
+  command => "/root/sup/mget_if_changed.sh /joyentsup/stor/BOP/mantaRun /opt/BOP/mantaRun",
 }
 
 
 exec { "mget-mantaenv.sh":
-  command => "/root/sup/mget_if_changed.sh /linda/stor/BOP/mantaenv.sh /opt/BOP/mantaenv.sh",
+  command => "/root/sup/mget_if_changed.sh /joyentsup/stor/BOP/mantaenv.sh /opt/BOP/mantaenv.sh",
 }
 
 
 exec { "mget-opscheck":
-  command => "/root/sup/mget_if_changed.sh /linda/stor/BOP/opscheck /opt/BOP/opscheck",
+  command => "/root/sup/mget_if_changed.sh /joyentsup/stor/BOP/opscheck /opt/BOP/opscheck",
 }
 
 
 exec { "mget-poseidonRun":
-  command => "/root/sup/mget_if_changed.sh /linda/stor/BOP/poseidonRun /opt/BOP/poseidonRun",
+  command => "/root/sup/mget_if_changed.sh /joyentsup/stor/BOP/poseidonRun /opt/BOP/poseidonRun",
 }
 
 
 exec { "mget-sshconfig.txt":
-  command => "/root/sup/mget_if_changed.sh /linda/stor/BOP/sshconfig.txt /opt/BOP/sshconfig.txt",
+  command => "/root/sup/mget_if_changed.sh /joyentsup/stor/BOP/sshconfig.txt /opt/BOP/sshconfig.txt",
 }
 
 
 exec { "mget-testmain.sh":
-  command => "/root/sup/mget_if_changed.sh /linda/stor/BOP/testmain.sh /opt/BOP/testmain.sh",
+  command => "/root/sup/mget_if_changed.sh /joyentsup/stor/BOP/testmain.sh /opt/BOP/testmain.sh",
 }
 
 
 exec { "mget-thothRun":
-  command => "/root/sup/mget_if_changed.sh /linda/stor/BOP/thothRun /opt/BOP/thothRun",
+  command => "/root/sup/mget_if_changed.sh /joyentsup/stor/BOP/thothRun /opt/BOP/thothRun",
 }
 exec { "install-manta-hk":
   command => "/usr/bin/cd /opt && /opt/local/bin/git clone git@github.com:joyent/manta-hk.git && /usr/bin/cd /opt/manta-hk && npm install",
